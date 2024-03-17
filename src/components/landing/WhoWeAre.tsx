c2caf1dd1028ce7f1ede3410e8589ea342b4a223
EmtiazHossainE2 <emtiazemon310@gmail.com>
2024-03-17 23:33:58 +0600

    d

--- src/components/landing/WhoWeAre.tsx
+++ src/components/landing/WhoWeAre.tsx
@@ -20,7 +20,7 @@ function WhoWeAre() {
         <img
           src={currentImage?.image}
           alt={currentImage?.title}
-          className='h-[600px] '
+          className='lg:h-[600px] '
         />
       </div>
 
